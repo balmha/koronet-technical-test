036a17ac505f2a4a46a7150a1d20d098b7a92d5b
Nicolas Balmaceda <balmacedanicolas4@gmail.com>
2024-09-27 14:08:35 -0300

    Run docker-compose successfully

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # Description: Dockerfile for the nodejs app. The nodejs app is a simple app that listens on port 3000 and returns a simple message. The webserver files are located at ./webserver folder
 
-FROM node:10
+FROM node:18-alpine
 WORKDIR /usr/src/app
 COPY webserver/ ./
 RUN npm install
--- webserver/app.js
+++ webserver/app.js
@@ -1,20 +1,18 @@
-//write a node.js server that listens on port 3000 and serves the following html file. The server should only serve this file for the exact request url './index.html'. All other request urls should return a 404 error.
-// The node.js server  should connect to a redis database which will be included in the docker-compose setup
-
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
+// This function return index.html file in case that the path is / or /index.html
 http.createServer(function (req, res) {
-    if (req.url === './index.html') {
+    if (req.url === '/index.html' || req.url === '/') {
         fs.readFile(path.join(__dirname, 'index.html'), function (err, data) {
-        if (err) {
-            res.writeHead(500);
-            res.end('Error loading index.html');
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        }
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading index.html');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
         });
     } else {
         res.writeHead(404);
@@ -27,9 +25,15 @@ console.log('Server running at http://localhost:3000/');
 
 // Create a connection to a local Redis instance
 var redis = require('redis');
-var client = redis.createClient(6379, 'redis');
-// Set redis credentials
-client.auth('password');
+var client = redis.createClient({
+    port: 6379,
+    host: 'redis',
+    password: 'password'
+});
+
+client.on('error', function (err) {
+    console.log('Redis error: ' + err);
+});
 // Set a value
 client.set('my test key', 'my test value', redis.print);
 // Retrieve a value
